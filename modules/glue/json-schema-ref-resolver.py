38dfa820611aa3d42f284f24ade2afe513c8ab2a
Santiago Bacaro <sbacarob@outlook.com>
2023-05-26 12:39:23 -0500

    try thing with json var

--- modules/glue/json-schema-ref-resolver.py
+++ modules/glue/json-schema-ref-resolver.py
@@ -1,2 +1,18 @@
 import os
+import argparse
+import json
+
 print(os.system("ls modules/glue/cloud-api-specs"))
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Recursively resolve all $ref references in the provided schema"
+    )
+    parser.add_argument("-p", "--paths", type=str, help="", required=True)
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    file_paths = json.loads(args.paths)
+
+    print(file_paths)
